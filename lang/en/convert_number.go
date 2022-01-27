8d4199307c79be1108f894d42e75db2d575dfba7
gammban <gammban@gmail.com>
2022-01-27 17:03:19 +0600

    add comments to en

--- lang/en/convert_number.go
+++ lang/en/convert_number.go
@@ -2,6 +2,8 @@ package en
 
 import "github.com/gammban/numtow/internal/ds"
 
+// Float64 converts float64 number to english words or returns error.
+//  result, err := en.Float64(-99.124) // result: minus ninety-nine point one hundred and twenty-four
 func Float64(decimal float64, options ...OptFunc) (result string, err error) {
 	o := prepareOptions(options...)
 
@@ -13,6 +15,8 @@ func Float64(decimal float64, options ...OptFunc) (result string, err error) {
 	return convertDecimal(intDS, fracDS, options...)
 }
 
+// MustFloat64 converts float64 number to english words or returns empty string.
+//  result := en.MustFloat64(-99.124) // result: minus ninety-nine point one hundred and twenty-four
 func MustFloat64(decimal float64, options ...OptFunc) string {
 	result, err := Float64(decimal, options...)
 	if err != nil {
@@ -22,6 +26,8 @@ func MustFloat64(decimal float64, options ...OptFunc) string {
 	return result
 }
 
+// String converts string number to english words or returns error.
+//  result, err := en.String("21") // result: twenty-one
 func String(decimal string, options ...OptFunc) (result string, err error) {
 	o := prepareOptions(options...)
 
@@ -33,6 +39,10 @@ func String(decimal string, options ...OptFunc) (result string, err error) {
 	return convertDecimal(intDS, fracDS, options...)
 }
 
+// MustString converts string number to english words or returns empty string.
+//  en.MustString("21") // result: "twenty-one"
+//  en.MustString("bad") // result: ""
+//  en.MustString("-99.124") // result: "minus ninety-nine point one hundred and twenty-four"
 func MustString(decimal string, options ...OptFunc) string {
 	result, err := String(decimal, options...)
 	if err != nil {
@@ -42,10 +52,14 @@ func MustString(decimal string, options ...OptFunc) string {
 	return result
 }
 
+// Int64 converts int64 number to english words or returns error.
+//  result, err := en.Int64(1) // result: one
 func Int64(num int64, options ...OptFunc) (result string, err error) {
 	return convert(ds.ParseInt64(num))
 }
 
+// MustInt64 converts int64 number to english words or returns empty string.
+//  result, err := en.MustInt64(1) // result: one
 func MustInt64(num int64, options ...OptFunc) string {
 	result, err := Int64(num, options...)
 	if err != nil {
